f48aeb5cd4680de0447e161e62d3fc20fca3d31e
b1koshtye <220103042@stu.sdu.edu.kz>
2026-04-21 02:32:09 +0500

    fix CLIENT_URL env

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
-require("dotenv").config();
-
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 
 const config = require("./config");
 const mongoose = require("mongoose");
