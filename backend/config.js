218777e53aecb01be8392e4ed1bd06cd528955a7
b1koshtye <220103042@stu.sdu.edu.kz>
2026-04-27 23:28:12 +0500

    some small fixes

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,5 @@
 const config = {
-  openaiKey: "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"
+  openaiKey: process.env.OPENAI_API_KEY
 };
 
 module.exports = config;
